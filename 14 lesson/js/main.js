70c83e3caee1087b94c3d8c5c6ac2f4749d577b8
student <student@BTU.local>
2019-10-13 13:56:22 +0400

    Lessons

---
+++ 14 lesson/js/main.js
@@ -0,0 +1,11 @@
+    // // console.log (typeof YEAR);
+    // //  console.log (typeof year);
+    // //  console.log (typeof year    );
+
+    // console.log("30" * 5);
+    // console.log(1 / 0);
+    // console.log(-1 / 0);
+    // let str_1 = "String 1";
+    // let str_2 = "`String 2`";
+    // let str_3 = "String 3"
+    // console.log("Welcome" + str_1)
---
+++ 15 lesson/js/main.js
@@ -0,0 +1,8 @@
+// let age = 24;
+// // console.group(age)
+
+// let checked = false;
+// console.log (checked , typeof checked)
+
+console.log (Number("2019" )
+console.log (newAge < age ))
