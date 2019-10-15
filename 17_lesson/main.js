ce796a5be0dbcdba32e27d0e0f21b8e17b465477
student <student@BTU.local>
2019-10-15 21:47:40 +0400

    ads

---
+++ 17_lesson/main.js
@@ -0,0 +1,33 @@
+// let number = parseInt(prompt("Number:", 0));
+// const guess = 8;
+
+// const MIN = 7;
+// const MAX = 14;
+
+// if (number > MIN && number < MAX) {
+//   alert("You Won!...");
+// } 
+// else if (number < MAX && number % 2 == 1){
+//     alert ("WOW!...")
+// }
+// else {
+//   alert("Loser");
+// }
+// console.log(number)
+
+// const result = ( guess == number ) ? "Moige!..." : "Waage!...";
+// alert(result)
+
+let text = prmpt ("username");
+if (text == "Admin"){
+prompt ("password");
+}
+else if (text != "Admin"){
+    prompt ("I dont know you");
+}
+else {
+    alert("Cancel")
+}
+
+
+
